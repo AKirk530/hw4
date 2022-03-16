ec717d48d67b8b54d8f39565514935209101eaa2
AKirk530 <alex.kirk@kellogg.northwestern.edu>
2022-03-16 01:38:24 +0000

    Commit 11

--- app/controllers/activities_controller.rb
+++
@@ -1,8 +0,0 @@
-class ActivitiesController < ApplicationController
-    def create
-        @activty = Activity.new(params["activity"])
-        @activity.user_id = @current_user.id
-        @activity.save
-        redirect_to "/places/#(@activity.user_id)"
-    end
-end
